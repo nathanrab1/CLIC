e18492e973c46174078f7f1e95fb5f0e71efddee
nathanrab1 <nathanrab@gmail.com>
2025-03-26 19:14:05 -0300

    26/3/2025 CLIC single unidades layout parte 3

--- clic/app/public/wp-content/themes/clic/single-unidades.php
+++ clic/app/public/wp-content/themes/clic/single-unidades.php
@@ -11,7 +11,7 @@ get_header();
 ?>
 
 
-    <section class="fx fx-col fx-d-row destaque g-30 pb-25 fx-d-jcsb fx-d-aic">
+    <section class="fx fx-col fx-d-row destaque g-30 pb-25 fx-d-jcsb pt-d-25">
         <!-- Vídeo Geral --------------------------------------------->
         <iframe class="col-d-45" src="https://www.youtube.com/embed/<?php echo get_field('codigo_youtube_video_curto'); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         
@@ -24,7 +24,7 @@ get_header();
             <div class="fx fx-col g-20">
                 <!-- Parágrafo Curto------------------------------------------->
                 <?php if(get_field('paragrafo_curto')):?>
-                    <p class="t-5"><?php echo get_field('paragrafo_curto'); ?></p>
+                    <p class="t-4"><?php echo get_field('paragrafo_curto'); ?></p>
                 <?php endif;?>
                 
                 <ul class="fx fx-row g-10">
@@ -41,61 +41,60 @@ get_header();
         </div>
     </section>
 
-    <section class="fx fx-col g-25 p-25 wrapper">
-    <!-- Compreensões Centrais----------------------------------->
-            <h1 class="t-3 t-bold">Compreensões Centrais</h1>
-            <div class="t-4 t-wys"><?php echo get_field('ideias_centrais'); ?></div>
-    </section>
-    
-    <section class="fx fx-col g-25 p-25 wrapper">
-    <!-- Habilidades BNCC----------------------------------->
-        <h1 class="t-3 t-bold">Habilidades BNCC</h1>
-        <div class="t-4 t-wys"><?php echo get_field('habilidades_bncc'); ?></div>
-    </section>
 
-    <section class="fx fx-col g-25 p-25 wrapper">
-    <!-- Evidências de Aprendizagem----------------------------------->
-        <h1 class="t-3 t-bold">Evidências de Aprendizagem</h1>
-        <div class="t-4 t-wys"><?php echo get_field('evidencias_de_aprendizagem'); ?></div>   
-    </section>
+      
+        <section class="fx fx-col fx-d-row g-25 p-25 wrapper fx-jcsb">
+        <!-- Compreensões Centrais----------------------------------->
+            <h1 class="t-2 t-bold col-d-25">Compreensões Centrais</h1>
+            <div class="t-4 t-wys col-d-66"><?php echo get_field('ideias_centrais'); ?></div>
+        </section>
+        
+        <section class="fx fx-col fx-d-row g-25 p-25 wrapper fx-jcsb">
+        <!-- Habilidades BNCC----------------------------------->
+            <h1 class="t-2 t-bold col-d-25">Habilidades BNCC</h1>
+            <div class="t-4 t-wys col-d-66"><?php echo get_field('habilidades_bncc'); ?></div>
+        </section>
+
+        <section class="fx fx-col fx-d-row g-25 p-25 wrapper fx-jcsb">
+        <!-- Evidências de Aprendizagem----------------------------------->
+            <h1 class="t-2 t-bold col-d-25">Evidências de Aprendizagem</h1>
+            <div class="t-4 t-wys col-d-66"><?php echo get_field('evidencias_de_aprendizagem'); ?></div>   
+        </section>
   
 
-
-
-
-    
+   
     <!-- Encontros ------------------------------->
     <?php if ( have_rows( 'encontros' ) ) :
         $count = 1; 
     ?>
         <section class="fx fx-col g-25 p-25 wrapper">
-            <h1 class="t-3">Encontros</h1>
-            <ul class="lista-encontros fx fx-col g-10">
+            <h1 class="t-3 t-bold">Encontros</h1>
+            <ul class="fx fx-col g-10">
             <?php while ( have_rows( 'encontros' ) ) : the_row(); ?>
                 <li class="encontro-container">
 
                     <div class="encontro-title bb-preto i-seta-dir fx fx-row fx-aic fx-jcsb">
-                        <h2>Encontro <?php echo $count; $count++;?></h2>
+                        <h2 class="t-4">Encontro <?php echo $count; $count++;?></h2>
                     </div>
 
                     <div class="unidade-encontro-content p-25">
                         
-                        <h2 class=" t-3"><?php echo get_sub_field( 'titulo' ); ?></h2>  
+                        <h2 class=" t-3 t-bold"><?php echo get_sub_field( 'titulo' ); ?></h2>  
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo get_sub_field( 'codigo_youtube' ); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> 
                         
                         <section class="fx fx-col g-10 p-25">
-                            <h2 class="t-3">Objetivos de Aprendizagem</h2>
+                            <h2 class="t-3 t-bold ">Objetivos de Aprendizagem</h2>
                             <div class="t-4 t-wys"><?php echo get_sub_field( 'objetivos' ); ?></div>
                         </section>
                         
                         <section class="fx fx-col g-10 p-25">
-                            <h2 class="t-3">Materiais e Ferramentas</h2>
+                            <h2 class="t-3 t-bold ">Materiais e Ferramentas</h2>
                             <div class="t-4 t-wys"><?php echo get_sub_field( 'materiais_e_ferramentas' ); ?></div>
                         </section>
 
                         <section class="fx fx-col g-10 p-25">
                             <h2 class="t-3 t-bold">Principais Etapas</h2>
-                            <div class="t-wys fx fx-col g-20"><?php echo get_sub_field( 'principais_etapas' ); ?></div>
+                            <div class="t-4 t-wys fx fx-col g-20"><?php echo get_sub_field( 'principais_etapas' ); ?></div>
                         </section>
 
                     </div>
