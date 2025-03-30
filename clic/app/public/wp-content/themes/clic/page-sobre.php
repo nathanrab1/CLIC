aa9f0fdc5636c61921b6154778f794d4635d0fc2
nathanrab1 <nathanrab@gmail.com>
2025-03-29 22:26:31 -0300

    29/3/2025 CLIC page sobre layout

--- clic/app/public/wp-content/themes/clic/page-sobre.php
+++ clic/app/public/wp-content/themes/clic/page-sobre.php
@@ -16,18 +16,30 @@
 get_header();
 ?>
 
-<div class="sobre-container">
+    <h1 class="t-1 wrapper pt-25">Sobre o CLIC</h1>
     
-    <div class="unidade-titulo-div">
-        <h1 class="unidade-título">Sobre o CLIC</h1>
-    </div>
+    <section class="fx-d fx-d-row destaque-sobre wrapper-d pt-25 fx-jcsb">
+        <h2 class="t-3 col-d-33 wrapper-m pb-25 "><?php echo get_field('texto_sobre_1'); ?></h2>
+        <iframe class="col-d-58" src="https://www.youtube.com/embed/<?php echo get_field('codigo_video_youtube_sobre'); ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </section>
 
+    <p class="t-3 wrapper p-25"><?php echo get_field('texto_sobre_2'); ?></p>
+    <p class="t-3 wrapper pb-25"><?php echo get_field('texto_sobre_3'); ?></p>
+
+    <section class="fx fx-col fx-d-row g-25 p-25 wrapper fx-jcsb">
+        <h2 class="t-3 t-bold col-d-25">O que fazemos?</h2>
+        <h3 class="t-3 t-wys col-d-66"><?php echo get_field('texto_sobre_4'); ?></h3>
+    </section>
+
+    <section class="fx fx-col fx-d-row g-25 p-25 wrapper fx-jcsb">
+        <h2 class="t-3 t-bold col-d-25"><?php echo get_field('texto_sobre_5'); ?></h2>
+        <div class="t-3 t-wys col-d-66"><?php echo get_field('sobre_bullet_points'); ?></div>
+    </section>
+
+    <p class="t-3 wrapper pb-45"><?php echo get_field('texto_sobre_6'); ?></p>
+     
     
-    <div class="wrapper-sobre-texto">
-        <p class=""><?php echo get_field('texto_sobre'); ?></p> 
-    </div>
 
-</div>
 
 <?php
 get_footer();
