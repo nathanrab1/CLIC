4bcf210897f84975701a0653b23264784b4927b4
nathanrab1 <nathanrab@gmail.com>
2025-04-04 18:14:05 -0300

    4/4/2025 home parte 2

--- app/public/wp-content/themes/clic/page-home.php
+++ app/public/wp-content/themes/clic/page-home.php
@@ -14,40 +14,40 @@
 
 get_header();
 ?>
-<section class="wrapper fx fx-d-row fx-col fx-jcc fx-aife g-30">
-    <div>
-        <img class="home-image-logo" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/logo-clic.svg" alt="Logo">
-        <h1 class="t-1">Transforme o currículo a partir da computação</h1>  
+<section class="wrapper fx fx-d-row fx-col fx-jcc fx-aife g-30 pb-25">
+    <div class="fx-d fx-d-row pt-30">
+        <img class="col-d-30 home-image-logo" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/logo-clic.svg" alt="Logo">
+        <h1 class="col-d-30 t-1 t-azul t-center">Transforme o currículo a partir da computação</h1>  
     </div>
-    <h2 class="t-3 home-paragrafo"><?php echo get_field( 'paragrafo_home' ); ?></h2>
+    <h2 class="t-3 home-paragrafo t-center"><?php echo get_field( 'paragrafo_home' ); ?></h2>
 </section>
 
 
 
-<section class="wrapper-home home-imagem fx fx-row g-20 fx-jcc">
-    <div class="fx-d fx-row col-d-40 g-30 fx-aic">
-        <img class="col-d-30" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/home_icon_1.svg" alt="icon 1">
+<section class="wrapper-home home-imagem fx fx-row fx-jcc">
+    <div class="fx fx-d-row fx-col col-d-40 fx-aic t-center p-25">
+        <img class="col-d-30 pb-10" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/home_icon_1.svg" alt="icon 1">
         <div class="col-d-60">
             <h2 class="t-4 t-bold p-10"><?php echo get_field( 'titulo_paragrafo_icone_1' ); ?></h2>
             <h3 class="t-4"><?php echo get_field( 'paragrafo_icone_1' ); ?></h3>
         </div>
     </div>
-    <div class="fx-d fx-row col-d-40 g-30 fx-aic">
-        <img class="col-d-30" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/home_icon_2.svg" alt="icon 2">
+    <div class="fx fx-d-row fx-col col-d-40 fx-aic t-center p-25">
+        <img class="col-d-30 pb-10" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/home_icon_2.svg" alt="icon 2">
         <div class="col-d-60">
             <h2 class="t-4 t-bold p-10"><?php echo get_field( 'titulo_paragrafo_icone_2' ); ?></h2>
             <h3 class="t-4"><?php echo get_field( 'paragrafo_icone_2' ); ?></h3>
         </div>
     </div>
-    <div class="fx-d fx-row col-d-40 g-30 fx-aic">
-        <img class="col-d-30" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/home_icon_3.svg" alt="icon 3">
+    <div class="fx fx-d-row fx-col col-d-40 fx-aic t-center p-25">
+        <img class="col-d-30 pb-10" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/home_icon_3.svg" alt="icon 3">
         <div class="col-d-60">
             <h2 class="t-4 t-bold p-10"><?php echo get_field( 'titulo_paragrafo_icone_3' ); ?></h2>
             <h3 class="t-4"><?php echo get_field( 'paragrafo_icone_3' ); ?></h3>
         </div>
     </div>
-    <div class="fx-d fx-row col-d-40 g-30 fx-aic">
-        <img class="col-d-30" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/home_icon_4.svg" alt="icon 4">
+    <div class="fx fx-d-row fx-col col-d-40 fx-aic t-center p-25">
+        <img class="col-d-30 pb-10" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/home_icon_4.svg" alt="icon 4">
         <div class="col-d-60">
             <h2 class="t-4 t-bold p-10"><?php echo get_field( 'titulo_paragrafo_icone_4' ); ?></h2>
             <h3 class="t-4"><?php echo get_field( 'paragrafo_icone_4' ); ?></h3>
